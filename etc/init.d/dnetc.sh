7f49f77ae9080d36e66d80222e9e54a1ba2eabe2
Eugene E. Kashpureff <eugenekay@kashpureff.org>
2011-09-26 04:59:48 -0500

    Add some stuff
    *Rudimentary init script for Distributed.net client, dnetc config file
    *Rudimentary init script for ZNC
    *Notes on installing SL6

---
+++ etc/init.d/dnetc.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# /etc/init.d/dnetc
+#
+# Init file for Distributed.net client
+#
+# chkconfig: 2345 55 25
+# description: Distributed.net client
+#
+# processname: dnetc
+#
+# You need to specify DNETC_USER and DNETC_INI in /etc/sysconfig/dnetc!
+#
+
+prog=dnetc
+DNETC_BIN=/usr/bin/dnetc
+DNETC_CONFIG=/etc/sysconfig/$prog
+
+[ -x $DNETC_BIN ] || exit 0
+
+[ -f $DNETC_CONFIG ] && . "$DNETC_CONFIG"
+
+#[ id $DNETC_USER ] || exit 0
+
+[ -f $DNETC_INI ] || exit 0
+
+# Source function library
+. /etc/rc.d/init.d/functions
+
+RETVAL=0
+
+start()
+{
+	echo -n $"Starting $prog: "
+	pids=`pidof dnetc | tr " " "\n"`
+	for pid in $pids
+	do
+		if [ -e /proc/$pid ];
+		then
+			runs=1
+		fi
+	done
+
+	if [ $runs ]; then
+		echo -n $"already running.";
+		success "$prog is already running.";
+		echo
+		return 0
+	fi
+	daemon su $DNETC_USER --command=\"$DNETC_BIN -quiet -ini $DNETC_INI\"
+	# The \" matter, they make su interpret the contents, rather than daemon
+	RETVAL=$?
+	echo
+	return $RETVAL
+}
+
+stop()
+{
+	echo -n $"Shutting down $prog: "
+	killproc $DNETC_BIN
+	# NOTE: This currently kills ALL running DNETC instances on the machine
+	# Please fix this, preferably by checking for pidfile.
+	RETVAL=$?
+	echo
+	rm -f $pidfile
+	return $RETVAL
+}
+
+reload()
+{
+	echo -n $"Reloading $prog configuration: "
+	killproc $DNETC_BIN -HUP
+	RETVAL=$?
+	echo
+	return $RETVAL
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	reload)
+		reload
+		;;
+	flush )
+		flush
+		;;
+	restart)
+		stop
+		start
+		;;
+	try-restart)
+		if [ -f $pidfile ]; then
+			stop
+			start
+		fi
+		;;
+	force-reload)
+		reload || (stop; start)
+		;;
+	status)
+		status $prog
+		RETVAL=$?
+		;;
+	*)
+		echo $"Usage: $0 {start|stop|reload|force-reload|restart|try-restart|status}"
+		RETVAL=3
+esac
+
+exit $RETVAL
