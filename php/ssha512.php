a71db52bebd579b2dc3d63e7587cdb08c2815848
Eugene E. Kashpureff <eugene@kashpureff.org>
2011-09-27 00:23:23 -0500

    Make SSHA512 standards-compliant.

--- php/ssha512.php
+++ php/ssha512.php
@@ -1,28 +1,80 @@
 <?php
-# ssha512.php
+// ssha512.php
+
+/*
+// Generate a SSHA512(Salted SHA-2 512bit) hash from a given input text. 
+//
+// Copyright 2011 Eugene E. "EugeneKay" Kashpureff (eugene@kashpureff.org)
+// Licensed under GNU General Public License, version 3 or later.
+*/
 ?>
-<!--
-# Generate a SSHA512(Salted SHA-2 512bit) hash from a given input text. 
-#
-# Copyright 2011 Eugene E. "EugeneKay" Kashpureff (eugene@kashpureff.org)
-# Licensed under GNU General Public License, version 3 or later.
-#
+<!-- 
+Generate a SSHA512(Salted SHA-2 512bit) hash from a given input text. 
+
+Copyright 2011 Eugene E. "EugeneKay" Kashpureff (eugene@kashpureff.org)
+Licensed under GNU General Public License, version 3 or later.
 -->
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8" /> 
 <title>Salted SHA-512 Generator</title>
+<style type="text/css">
+a:link,
+a:visited {
+	text-decoration: none;
+	color: #00f;
+}
+a:hover,
+a:active {
+	text-decoration: none;
+	color: #f00;
+}
+fieldset {
+	margin: 0;
+	border: 0;
+	padding: 0;
+}
+#header {
+	text-align: center;
+	font-size: 4em;
+}
+#passform {
+	text-align: center;
+	margin: 2em 2em 2em 2em;
+}
+#footer {
+	position: fixed;
+	bottom: 0px;
+	width: 99%;
+}
+#footer div {
+	width: 500px;
+	position: static;
+	
+	margin: 0 auto 0 auto;
+	border-style: solid;
+	border-width: 1px 1px 0 1px;
+	border-color: #000;
+	padding: 5px;
+	text-align: center;
+	font-size: 1.5em;
+}
+</style>			
 </head>
 <body>
-<center><h1>Salted SHA-512 Generator</h1></center>
-<br>
-<br>
-<center>Source code available on <a href="https://github.com/EugeneKay/scripts/blob/master/php/ssha512.php">GitHub</a></center>
-<br>
-<br>
-<form action="ssha512.php" method="POST">
-<center><input type="password" name="password" size="40" value=""><br>
-<input type="submit" value="Hash!"></center>
+<div id="header">SSHA512 Generator</div>
+<p id="passform">
+<object>
+<form action="ssha512.php" method="post">
+<fieldset>
+<input type="password" name="password" size="40" value="" />
+<input type="submit" value="Hash!" />
+</fieldset>
 </form>
+</object>
+</p>
 
 <?php
 if (isset($_POST['password'])) {
@@ -36,6 +88,6 @@ if (isset($_POST['password'])) {
 	echo "</font></center>";
 }
 ?>
-
+<div id="footer"><div>Source code available on <a href="https://github.com/EugeneKay/scripts/blob/master/php/ssha512.php">GitHub</a></div></div>
 </body>
 </html>
