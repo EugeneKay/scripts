3ec3e672958caacb5d19006278097a7ea43eb0d0
Eugene E. Kashpureff <eugene@kashpureff.org>
2011-10-24 03:19:51 -0500

    Add features & fix formatting
    *Add TXT record with time of update
    *Add SSHFP records with host's ssh pubkeys
    *Change spaces to tabs
    *Add missing tabs

--- bash/nsupdater.sh
+++ bash/nsupdater.sh
@@ -5,7 +5,7 @@
 #
 # NS Updater is a script which checks your current public IP address against the
 # existing record, and updates it if necessary using a TSIG key. Will eventually
-# support IPv6 updates and additional record types(such as SSHFP) as well.
+# support IPv6 updates.
 #
 # Copyright 2011 Eugene E. Kashpureff (eugene@kashpureff.org)
 # License: WTFPL, any version or GNU General Public License, version 3+
@@ -49,25 +49,36 @@ dnsip=$(dig +short @${SERVER} ${RECORD}.${DOMAIN} A)
 # Check the Public IP against the DNS IP
 if [ "${pubip}" != "${dnsip}" ]
 then
-        # Build the update query
+	# Build the update query
 	# TODO: add prerequisites, update AAAA as well
-        update="server ${SERVER}\n"
-        update="${update}zone ${DOMAIN}\n"
-        update="${update}update delete ${RECORD}.${DOMAIN}\n"
-        update="${update}update add ${RECORD}.${DOMAIN} ${TTL} IN A ${pubip}\n"
-        update="${update}send\n"
-
-        # Run the update
-        echo -e ${update} | nsupdate -y ${KEYNAME}:${KEYSECRET}
-
-        # Check that the update went through
-        newip=$(dig a ${RECORD}.${DOMAIN} +short)
-        if [ "${pubip}" != "${newip}" ]
-        then
-                echo "Failed to update ${RECORD}.${DOMAIN} to ${pubip} using key ${KEYNAME}"
-        else
-                echo "Successfully updated ${RECORD}.${DOMAIN} to ${pubip}."
-        fi
+	update="server ${SERVER}\n"
+	update="${update}zone ${DOMAIN}\n"
+	update="${update}update delete ${RECORD}.${DOMAIN}\n"
+	update="${update}update add ${RECORD}.${DOMAIN} ${TTL} IN A ${pubip}\n"
+	update="${update}update add ${RECORD}.${DOMAIN} ${TTL} IN TXT \"Updated $(date +'%s %Y-%m-%d %0H:%M:%S %:z(%Z)')\"\n"
+	if [ -f /etc/ssh/ssh_host_rsa_key.pub ]
+	then
+		rsakey=$(cat /etc/ssh/ssh_host_rsa_key.pub | cut -d ' ' -f 2 | openssl base64 -d -A | openssl sha1 | cut -d ' ' -f 2)
+		update="${update}update add ${RECORD}.${DOMAIN} ${TTL} IN SSHFP 1 1 ${rsakey}\n"
+	fi
+	if [ -f /etc/ssh/ssh_host_dsa_key.pub ]
+	then
+		dsakey=$(cat /etc/ssh/ssh_host_dsa_key.pub | cut -d ' ' -f 2 | openssl base64 -d -A | openssl sha1 | cut -d ' ' -f 2)
+		update="${update}update add ${RECORD}.${DOMAIN} ${TTL} IN SSHFP 2 1 ${dsakey}\n"
+	fi
+	update="${update}send\n"
+	
+	# Run the update
+	echo -e ${update} | nsupdate -y ${KEYNAME}:${KEYSECRET}
+	
+	# Check that the update went through
+	newip=$(dig +short @${SERVER} ${RECORD}.${DOMAIN} A)
+	if [ "${pubip}" != "${newip}" ]
+	then
+		echo "Failed to update ${RECORD}.${DOMAIN} to ${pubip} using key ${KEYNAME}"
+	else
+		echo "Successfully updated ${RECORD}.${DOMAIN} to ${pubip}."
+	fi
 fi
 
 ## Cleanup
@@ -76,6 +87,6 @@ fi
 unset DOMAIN RECORD KEYNAME KEYSECRET SERVER TTL
 
 # Unset script variables
-unset pubip dnsip update newip
+unset pubip dnsip update newip rsakey dsakey
 
 # TODO: give a meaningful exit status
