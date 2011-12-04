f8581849d1307bbb7cc53245285bd5b626d6a3b8
Eugene E. Kashpureff <eugene@kashpureff.org>
2011-12-04 04:59:47 -0600

    Add OpenVPN helper scripts for `ip`

---
+++ bash/ip-unpriv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##
+## IP Unpriv
+##
+#
+# IP Unpriv is a helper script for OpenVPN when running in unprivileged mode. It
+# is used to call IP Wrap via sudo, where additional checks can be performed
+# prior to invoking the `ip` command.
+#
+# Copyright 2011 Eugene E. Kashpureff (eugene@kashpureff.org)
+# License: WTFPL, any version or GNU General Public License, version 3+
+#
+
+## Launch IP Wrap via sudo
+sudo /usr/local/sbin/ip-wrap $*
+
+## Exit with the given return status
+exit $?
---
+++ bash/ip-wrap.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+##
+## IP Wrap
+##
+#
+# IP Wrap is a wrapper for the `ip` command for OpenVPN running in unprivileged
+# mode. It allows usage of "ip addr add|del" and "ip link" on tun0-99, and the
+# "ip route add|del" command.
+#
+# Copyright 2011 Eugene E. Kashpureff (eugene@kashpureff.org)
+# License: WTFPL, any version or GNU General Public License, version 3+
+#
+
+## Check with ip subcommand is being requested
+case "$1" in
+"addr")
+	## Only allow devices tun0-tun99
+        if [[ "$4" =~ ^t(un|ap)[0-9]{1,2}$ ]]
+        then
+		## Only allow add and del commands
+                case "$2" in
+                "add") /sbin/ip $* ;;
+                "del") /sbin/ip $* ;;
+                *) /bin/false ;;
+                esac
+        else
+                /bin/false
+        fi
+        ;;
+"link")
+	## Only allow devices tun0-tun99
+        if [[ "$4" =~ ^t(un|ap)[0-9]{1,2}$ ]]
+        then
+                /sbin/ip $*
+        else
+                /bin/false
+        fi
+        ;;
+"route")
+        case "$2" in
+        "add") /sbin/ip $* ;;
+        "del") /sbin/ip $* ;;
+        *) /bin/false ;;
+        esac
+	;;
+*) /bin/false ;;
+esac
+
+## Exit using the return status of the command run
+exit $?
