3282ded81202ebbac45e66641429aadf035abe1a
Eugene E. Kashpureff Jr <eugene@kashpureff.org>
2012-03-04 14:26:58 -0600

    git-last improvements
    *Auto-size filename column
    *Add some comments
    *Doesn't actually work on files, just dirs
    *Improve the 'pretty' output of git-log

--- bash/git-last.sh
+++ bash/git-last.sh
@@ -3,7 +3,7 @@
 # git-last
 #
 # Shows the most recent commit for each file/folder in the current directory,
-# similar to GitHub's code-browser. Accepts a list of files/directories to be
+# similar to GitHub's code-browser. Accepts a list of directories to be
 # examined
 #
 
@@ -13,14 +13,39 @@ then
 	exit 1
 fi
 
-# List files to show info for
-for name in $(find $* -mindepth 1 -maxdepth 1 -type d | sort | sed 's/$/\//g' && find $* -mindepth 1 -maxdepth 1 -type f | sort)
+# Files/directories to list info for
+names=$(find $* -mindepth 1 -maxdepth 1 -type d | sort | sed 's/$/\//g' && find $* -mindepth 1 -maxdepth 1 -type f | sort)
+
+# How far to indent the commit info
+length=14
+
+# Find length of longest filename
+for name in ${names}
+do
+	if [ ${#name} -gt $length ]
+	then
+		length=${#name}
+	fi
+done
+
+# Pad length by 2 to allow for trailing colon
+length=$(( $length + 2 ))
+
+# Loop over each dir/file
+for name in ${names}
 do
 	# Skip .git dirs(TODO: make this work better)
 	if [ "${name}" == "./.git/" ]
 	then	
 		continue
 	fi
-	echo -ne ${name}":" | sed 's/\.\///g' | sed -e :a -e 's/^.\{1,16\}$/& /;ta'
-	echo $(git-log -1 --oneline -- ${name})
+	
+	# Show path/filename, padded
+	echo -ne ${name}":" | sed 's/\.\///g' | sed -e :a -e "s/^.\{1,${length}\}$/& /;ta"
+	
+	# Show last commit date, padded
+	echo "$(git-log -1 --pretty=tformat:'%cr%x09%h %s [%cn]' -- ${name})"
 done
+
+# Exit cleanly
+exit 0
