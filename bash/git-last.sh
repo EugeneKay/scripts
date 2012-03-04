31c489fd1ff0e2fc77cbe0e1f157e89ac6950fc5
Eugene E. Kashpureff Jr <eugene@kashpureff.org>
2012-03-04 16:46:19 -0600

    More git-last improvements
    *Functionize the core git_last loop
    *Pipe output of git_last through less for consistency with git commands
    *Make first-column spacing behave better

--- bash/git-last.sh
+++ bash/git-last.sh
@@ -16,8 +16,8 @@ fi
 # Files/directories to list info for
 names=$(find $* -mindepth 1 -maxdepth 1 -type d | sort | sed 's/$/\//g' && find $* -mindepth 1 -maxdepth 1 -type f | sort)
 
-# How far to indent the commit info
-length=14
+# Minimum length of first column
+length=8
 
 # Find length of longest filename
 for name in ${names}
@@ -28,24 +28,43 @@ do
 	fi
 done
 
-# Pad length by 2 to allow for trailing colon
-length=$(( $length + 2 ))
+# Trim length by one to account for stripped leading ./ and trailing :
+length=$(( $length - 1 ))
 
-# Loop over each dir/file
-for name in ${names}
-do
-	# Skip .git dirs(TODO: make this work better)
-	if [ "${name}" == "./.git/" ]
-	then	
-		continue
-	fi
+## git_last
+#
+# Give some information about the last commit on directories/files
+#
+# Returns: 0
+# Outputs: Three(4) columns containing:
+#	*Filename
+#	*Commit date(relative)
+#	*Commit hash(short)
+#	*Commit subject [Commiter]
+#
+function git_last() {
+	for name in ${names}
+	do
+		# Skip .git dirs(TODO: make this work better)
+		if [ "${name}" == "./.git/" ]
+		then	
+			continue
+		fi
+		
+		# Show path/filename, padded
+		echo -ne ${name}":" | sed 's/\.\///g' | sed -e :a -e "s/^.\{1,${length}\}$/& /;ta"
+		echo -ne '	'
+		
+		# Show last commit date, padded
+		echo "$(git-log -1 --pretty=tformat:'%cr%x09%h %s [%cn]' -- ${name})"
+	done
 	
-	# Show path/filename, padded
-	echo -ne ${name}":" | sed 's/\.\///g' | sed -e :a -e "s/^.\{1,${length}\}$/& /;ta"
-	
-	# Show last commit date, padded
-	echo "$(git-log -1 --pretty=tformat:'%cr%x09%h %s [%cn]' -- ${name})"
-done
+	# Return cleanly
+	return 0
+}
+
+# Run the git_last loop and feed through paginator
+git_last | less -FRSX
 
 # Exit cleanly
 exit 0
