a3208b533c93f32cfcae90e7f70e79adc7620595
Eugene E. Kashpureff Jr <eugene@kashpureff.org>
2012-03-05 13:53:50 -0600

    Add git-wrapper.sh

---
+++ bash/git-wrapper.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Shell wrapper for the 'git' binary to hook in custom subcommands
+#
+# Copyright 2011 K and H Research Company (support@khresear.ch)
+# License: WTFPL, any version or GNU General Public License, version 3+
+#
+
+# Check against list of custom subcommands
+case $1 in
+"lol")
+	git log --graph --all --pretty=tformat:'%x09%cr%x09%h%d %s' ${@:2}
+	;;
+"uno")
+	git status --untracked=no
+	;;
+*)
+	# Execute the git binary
+	/usr/bin/git $*
+	;;
+esac
+
+# Exit with git's exit code
+exit $?
