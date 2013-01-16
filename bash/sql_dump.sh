fba74939d75ef23806c53bec946d276b175e2ace
William Cahill-Manley <william@cahillmanley.com>
2013-01-15 16:07:27 -0800

    Support skipping multiple databases
    Something I needed - Feel free to accept or ignore.

--- bash/sql_dump.sh
+++ bash/sql_dump.sh
@@ -12,15 +12,20 @@
 dumpdir="/data/backups"
 timestamp=`date +"%Y-%m-%d_%H:%M"`
 
+
+skip=(mysql information_schema performance_schema template0)
+
 ## MySQL
 for db in `mysql -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 do
 	# Skip some databases
-	if [ ${db} = "information_schema" ]
-	then
-		continue
-	fi
-	
+	for skipped in "${skip[@]}"; do
+		if [[ "$skipped" == "$db" ]]
+		then
+			continue 2
+		fi
+	done
+
 	# Dump DB, run through gzip, save to file
 	mysqldump -eq -h localhost ${db} | gzip -c - > "${dumpdir}/mysql/${db}_${timestamp}.sql.gz"
 	
@@ -30,10 +35,12 @@ done
 for db in `psql -l | grep en_US | cut -d "|" -f 1`
 do
 	# Skip some databases
-	if [ ${db} = "template0" ]
-	then
-		continue
-	fi
+	for skipped in "${skip[@]}"; do
+		if [[ "$skipped" == "$db" ]]
+		then
+			continue 2
+		fi
+	done
 	
 	# Dump DB, run through gzip, save to file
         pg_dump ${db} | gzip -c - > "${dumpdir}/postgres/${db}_${timestamp}.sql.gz"
@@ -44,4 +51,4 @@ done
 find ${dumpdir}/mysql/ -type f -mtime +14 -exec rm {} \;
 find ${dumpdir}/postgres/ -type f -mtime +14 -exec rm {} \;
 
-unset dumpdir timestamp db
+unset dumpdir timestamp db skip skipped
