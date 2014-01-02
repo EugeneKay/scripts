d080dc50c82d72e11e4c075a40e64662f1ca1013
Eugene E. Kashpureff Jr <eugene@kashpureff.org>
2014-01-02 12:32:14 -0800

    Different eBay shortlink script

--- web/ebay.js
+++ web/ebay.js
@@ -1,72 +1,37 @@
-﻿// ==UserScript==
-// @name          eBay Item Link V2
-// @version       2.1.0.0
-// @date          2012/02/05
-// @namespace     http://userscripts.org/scripts/show/61353
-// @description   Share smaller links to eBay items.
-// @creator       Jesse Graffam
-// @contributor   Avi Finkel
-// @copyright     Jesse Graffam
-// @license       GPL v3; http://www.gnu.org/licenses/gpl-3.0.html
-// @id            8746c50f-301f-415f-b651-56896b9d2dd8
-// @require       http://usocheckup.redirectme.net/61353.js?maxage=7&minage=12
-// @include       http://*.ebay.tld/*
+// ==UserScript==
+// @name          eBay Item Shortener
+// @version       1.1a
+// @date          2013/05/23
+// @description   Make links smaller in Ebay
+// @creator       Omeryl
+// @license       WTFPL; http://www.wtfpl.net/
+// @include       http://*.ebay.com/*
+// @grant         none
+// @namespace     http://userscripts.org/scripts/show/168204
 // ==/UserScript==
 //
-// © Jesse Graffam
+// © Omeryl
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// version 3 as published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You can receive a copy of the GNU General Public License by
-// visiting http://www.gnu.org/licenses/gpl-3.0.html
+//        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+//                    Version 2, December 2004 
+
+// Copyright (C) 2004 Omeryl <omeryl@bassh.net> 
+
+// Everyone is permitted to copy and distribute verbatim or modified 
+// copies of this license document, and changing it is allowed as long 
+// as the name is changed. 
+
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 (function()
 {
-	var m_XPsnap_1 = document.evaluate("//div//div[@class='z_b']//table[@cellpadding='3']//tr//td[@valign='top']",
-		document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null
-	);
-	
-	if (m_XPsnap_1.snapshotLength > 0)
-	{
-		// RegEx to find the item number
-		const itemNumRegEx = /^(\d*)$/
-		
-		for (var i = 0; i < m_XPsnap_1.snapshotLength; i++) {
-			var td = m_XPsnap_1.snapshotItem(i);
-			
-			// check if the node starts with text
-			if (
-				td.childNodes &&
-				(td.childNodes.length > 0) &&
-				(td.childNodes.item(0).nodeType == 3)
-				)
-			{
-				// check if the node content is only numbers
-				if (itemNumRegEx.test(td.textContent))
-				{
-					// can has item number
-					var num = td.childNodes.item(0).data;
-					// create new DOM parts
-					var newDiv = document.createElement("div");
-					newDiv.innerHTML = '<A HREF="/itm/'
-						+ num
-						+ '">'
-						+ num
-						+ '</A><BR/>'
-						+ '<FORM><INPUT ID="g_eBayAuctionURL" TYPE="input" VALUE="'
-						+ 'http://' + window.location.host + '/itm/' + num
-						+ '" STYLE="font-size:7pt;width:80px;" ONCLICK="javascript:g_eBayAuctionURL.select();"/></FORM>';
-					// replace the unlinked text
-					td.replaceChild(newDiv,td.childNodes[0]);
-				}
-			}
-		}
-	}
+	$('a[href*="ebay.com/itm/"]').each(function() {
+		var url = String($(this).attr('href')).split('/').length-1;
+		url = $(this).attr('href').split('/')[url];
+		url = url.split("?")[0];
+		$(this).attr('href', "/itm/" + url);
+	});
 })();
